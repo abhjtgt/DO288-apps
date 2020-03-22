c52d2c41752cbaafc72eb8eeebdc4c593262f3c5
abhjtgt <abhijitdd@yahoo.co.in>
2020-03-22 23:02:30 +0100

    multi fixes

--- hello-java/run-app.sh
+++ hello-java/run-app.sh
@@ -3,9 +3,9 @@
 echo "Starting hello-java app..."
 echo "JVM options => $JAVA_OPTIONS"
 echo
-echo $JAVA_OPTIONS | sed "s/\(.\)/\1\n/g" | while read x
-do
-echo "* $x"
-sleep 30
-done;
+#echo $JAVA_OPTIONS | sed "s/\(.\)/\1\n/g" | while read x
+#do
+#echo "* $x"
+#sleep 30
+#done;
 java $JAVA_OPTIONS -jar /opt/app-root/bin/hello-java.jar
