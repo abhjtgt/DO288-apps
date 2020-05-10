e72cf066df8456a7dd2ea5d0d510e627fa68c8e3
Abhijit <abhijitdd@yahoo.co.in>
2020-05-10 09:10:45 -0400

    Php scale change 2

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,3 +1,3 @@
 <?php
-  print "VERSION 1 # HOST -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+  print "VERSION 2 ##HOST -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "     ** \n";
 ?>
