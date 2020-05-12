369593fa114ef43e0fa530c61531c32bfef73462
abhjtgt <abhijitdd@yahoo.co.in>
2020-05-12 18:29:08 +0200

    local commit

---
+++ jenkins/bayad/src/main/java/com/mayajal/App.java
@@ -0,0 +1,13 @@
+package com.mayajal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Apun ich BHAGWAN hai" );
+    }
+}
